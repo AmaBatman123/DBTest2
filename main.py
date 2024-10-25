2b8f90718d3de92312574471b22b3749bb284648
Valerii <amabatman.12@gmail.com>
2024-10-25 15:16:16 +0600

    Add data to tables

--- main.py
+++ main.py
@@ -7,10 +7,21 @@ with sqlite3.connect('person.db') as conn:
     FirstName TEXT,
     LastName TEXT,
     DepartmentID INTEGER,
-    DepartmentName TEXT
+    FOREIGN KEY(DepartmentID) REFERENCES Department(ID)
     )''')
 
     cursor.execute('''CREATE TABLE IF NOT EXISTS Departments(
-    DepartmentID INTEGER PRIMARY KEY AUTOINCREMENT,
+    DepartmentID INTEGER PRIMARY KEY,
     DepartmentName TEXT
     )''')
+
+    cursor.execute('''INSERT INTO Departments(DepartmentID, DepartmentName) 
+    VALUES (101, "HR"), (102, "IT"), (103, "Sales")
+    ''')
+
+    cursor.execute('''INSERT INTO Employee(FirstName, LastName, DepartmentID)
+    VALUES ("Замиров", "Камиль", 1), ("Аманов", "Рустам", 1),
+     ("Вашин", "Антон", 2), ("Пургов", "Марат", 2),
+      ("Алиев", "Денис", 3), ("Мамытова", "Арина", 3), ("Астарков", "Марат", 3)
+      ''')
+
